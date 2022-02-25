b08c9553df802fd270a54895d87775e125cff72c
supr4t1m <supratim12345.ss@gmail.com>
2022-02-25 12:32:00 +0530

    minor bug fix
    A minor bug that prevented from selecting district in the landing page, was fixed.

--- search-districts.js
+++ search-districts.js
@@ -384,7 +384,8 @@ function customSelect() {
         for (let j = 0; j < OptionsCount; j++) {
           if (selectEle.options[j].innerHTML == this.innerHTML) {
             selectEle.selectedIndex = j;
-            selectEle.onchange(); // now onchage function is mannually called from here
+            if (selectEle.onchange)
+              selectEle.onchange(); // now onchage function is mannually called from here
             selectedDiv.innerHTML = this.innerHTML;
             let selectedInDiv =
               this.parentNode.getElementsByClassName("same-as-selected");
